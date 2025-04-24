8691daee3ebafbeaa127bb5254d42cd941749806
Bandi Kanti Chaitanya <bandikantichaitanya2@gmail.com>
2025-04-24 14:18:52 +0530

    updated version

---
+++ src/components/Review.js
@@ -0,0 +1,58 @@
+import { normalizeSourceMap } from 'css-loader/dist/utils';
+import React, { useState } from 'react'
+
+function Review({data}) {
+    let [index,setIndex]=useState(0)
+    const { id, name, job, image, text } = data[index];
+
+    function checkIndex(number) {
+        if (number < 0) {
+          return 0; 
+        } else if (number >= data.length-1) {
+          return data.length-1; 
+        }
+        return number;
+      }
+
+      function handlePrevious() {
+        setIndex((index) => checkIndex(index - 1));
+      }
+      
+      function handleNext(index) {
+        // setIndex((prevIndex) => checkIndex(prevIndex + 1));
+        setIndex(index+1)
+        // console.log(index)
+      }
+
+      function handleRandom(){
+        let number=Math.floor(Math.random()*data.length)
+        
+        while(number===index){
+             number=Math.floor(Math.random()*data.length)
+        }
+        setIndex(number)
+      }
+
+  return (
+    <div>
+        {
+            // data.map((list,index)=>(
+                <div>
+                <img src={image} className='person-img'  style={{height:'100px',width:'auto'}} />
+                <h3 className="author" id={`author-${id}`} >{name}</h3>
+                <p className="job">{job}</p>
+                <p className="info">{text}</p>
+                <div style={{display:'flex',justifyContent:'space-between'}}  >
+                <button className="prev-btn" onClick={handlePrevious} >Previous</button>
+                  <button className="random-btn"  onClick={handleRandom} >Random</button>
+                 <button className="next-btn"  onClick={()=>{handleNext(index)}} disabled={index>=data.length-1} >Next</button>
+                </div>
+                </div>
+            // ))
+        }
+        
+    </div>
+  )
+}
+
+export default Review
